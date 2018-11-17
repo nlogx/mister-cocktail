0261c1daba064aec08764ed74502461843465f18
nlogx <lina.xiao@live.com>
2018-11-17 18:50:49 +0900

    devise finished setup

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_cocktail, only: %i[show destroy]
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/dashboard'
+  devise_for :users
   root to: 'cocktails#index'
   resources :cocktails, except: :index do
     resources :doses, only: %i[new create destroy]
